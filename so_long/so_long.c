ce8738ad6c3ff06592dd8d274457b987c8b1264f
Morne Barra <mbarra@ge-f6.kzn.21-school.ru>
2021-12-13 16:15:47 +0300

    2

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,18 +6,46 @@
 /*   By: mbarra <mbarra@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 17:25:59 by mbarra            #+#    #+#             */
-/*   Updated: 2021/12/10 19:45:29 by mbarra           ###   ########.fr       */
+/*   Updated: 2021/12/13 16:15:08 by mbarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+typedef struct	s_mlx
 {
 	void	*mlx;
-	void	*mlx_win;
+	void	*win;
+}				t_mlx;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1550, 550, "Hello world!");
-	mlx_loop(mlx);
+typedef	struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+}				t_img;
+
+int	key_hook(int keycode, t_mlx *mlx)
+{
+	printf("Hello from key_hook!\n");
+}
+
+
+int	main(void)
+{
+	t_mlx	mlx;
+	t_img	img;
+	void	*mlx_image;
+	void	*image;
+
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "Hello world!");
+	img.img = mlx_new_image(mlx.mlx, 400, 400);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght, &img.endian);
+	mlx_pixel_put(mlx.mlx, mlx.win, 40, 40, 0x00FF0000);
+	mlx_key_hook(mlx.win, key_hook, &mlx);
+	// mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 0, 0);
+	mlx_loop(mlx.mlx);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,12 +6,15 @@
 /*   By: mbarra <mbarra@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 18:26:18 by mbarra            #+#    #+#             */
-/*   Updated: 2021/12/10 19:31:45 by mbarra           ###   ########.fr       */
+/*   Updated: 2021/12/13 15:50:06 by mbarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # include <mlx.h>
+# include <stdio.h>
+# define WIN_HEIGHT	800
+# define WIN_WIDTH	600
 
 #endif
